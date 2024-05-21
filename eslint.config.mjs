abb98c270b528e0b41f1ad62572a4c22a4533d15
Daniele Rapisarda <info@danielerapisarda.com>
2024-05-21 13:20:19 +0200

    🔧 config eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import withNuxt from './.nuxt/eslint.config.mjs'
+import pluginJest from 'eslint-plugin-jest'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginVueA11y from 'eslint-plugin-vuejs-accessibility'
+
+const compat = new FlatCompat({
+  recommendedConfig: pluginJs.configs.recommended,
+})
+
+export default withNuxt([
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  pluginJest.configs.recommended['flat/recommended'],
+  ...pluginVueA11y.configs['flat/recommended'],
+  ...compat.config({
+    extends: [
+      '@nuxt',
+      'plugin:vue/vue3-essential',
+      'plugin:prettier/recommended',
+      'prettier',
+    ],
+  }),
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    ignores: ['node_modules'],
+    rules: {
+      'vue/multi-word-component-names': 0,
+      'vue/no-multiple-template-root': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+    },
+  },
+])
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: false,
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: /^~(.*)$/,
+          replacement: 'node_modules/$1',
+        },
+      ],
+    },
+  },
 })
