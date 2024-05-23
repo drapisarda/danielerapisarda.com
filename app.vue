b1e3850ff4d2a449b204d6a8d9e7dce1d4147b7f
Daniele Rapisarda <info@danielerapisarda.com>
2024-05-23 16:35:11 +0200

    ➕ vercel SpeedInsights

--- app.vue
+++ app.vue
@@ -2,11 +2,14 @@
   <NuxtLayout>
     <main>
       <NuxtPage />
+      <SpeedInsights />
     </main>
   </NuxtLayout>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+</script>
 
 <style lang="scss">
 @import '@/style/main.scss';
