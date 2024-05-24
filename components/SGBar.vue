504792a1d0d0e1da8571a6b2cc402f5db9b096e5
Daniele Rapisarda <info@danielerapisarda.com>
2024-05-24 14:13:18 +0200

    ✨ Serenegrove icons in home

--- components/SGBar.vue
+++ components/SGBar.vue
@@ -37,6 +37,7 @@ const logos: Logo[] = [
 
 .sgbar-bar {
   position: absolute;
+  z-index: -1;
   top: 0;
   left: 0;
   width: 100%;
@@ -57,24 +58,100 @@ const logos: Logo[] = [
     }
   }
 
+  li {
+    animation: float-x 16s ease-in-out infinite;
+  }
+
   svg {
     fill: $primary;
     opacity: 0.3;
-    animation: float 6s ease-in-out infinite;
+    animation: float-y 16s ease-in-out infinite;
+  }
+
+  li:nth-child(2) {
+    animation-delay: 0.5s;
+    animation-duration: 20s;
+    margin-left: -2rem;
+
+    svg {
+      animation-delay: 0.5s;
+      animation-duration: 20s;
+      margin-bottom: -2rem;
+    }
+  }
+
+  li:nth-child(3) {
+    animation-delay: 0s;
+    animation-duration: 20s;
+    margin-left: 3rem;
+    animation-direction: reverse;
+
+    svg {
+      animation-delay: 0s;
+      animation-duration: 20s;
+      margin-bottom: 3rem;
+      animation-direction: reverse;
+    }
+  }
+
+  li:nth-child(4) {
+    animation-delay: 0s;
+    animation-duration: 12s;
+    margin-left: -3rem;
+
+    svg {
+      animation-delay: 0s;
+      animation-duration: 12s;
+      margin-bottom: -3rem;
+    }
+  }
+
+  li:nth-child(5) {
+    animation-delay: 0s;
+    animation-duration: 13s;
+    margin-left: -12rem;
+
+    svg {
+      animation-delay: 0s;
+      animation-duration: 13s;
+      margin-bottom: -12rem;
+    }
   }
 }
 
-@keyframes float {
+@keyframes float-y {
   0% {
     transform: translatey(0px);
   }
 
-  50% {
-    transform: translatey(-100px);
+  33% {
+    transform: translatey(-30svh);
+  }
+
+  66% {
+    transform: translatey(30svh);
   }
 
   100% {
     transform: translatey(0px);
+  }
+}
+
+@keyframes float-x {
+  0% {
+    transform: translatex(0px);
+  }
+
+  33% {
+    transform: translatex(-20svh);
+  }
+
+  66% {
+    transform: translatex(20svh);
+  }
+
+  100% {
+    transform: translatex(0px);
   }
 }
 </style>
